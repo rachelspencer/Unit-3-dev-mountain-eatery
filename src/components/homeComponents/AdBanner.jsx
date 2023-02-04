6399efbb437f021728937aa3c538da39dbb1c085
Rachel Spencer <rachelspencer688@gmail.com>
2023-02-04 15:45:19 -0500

    AdBanner CSS added

--- src/components/homeComponents/AdBanner.jsx
+++ src/components/homeComponents/AdBanner.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const AdBanner = () => {
   return (
-    <div
+    <div 
       style={{
         background: `linear-gradient(
           190deg,
@@ -14,13 +14,13 @@ const AdBanner = () => {
         backgroundSize: "cover",
       }}
     >
-      <div>
+      <div className="banner">
         <h3>New Recipe</h3>
         <h1>Pineapple Salmon</h1>
-        <h3>
+        <p>
           This recipe consists of fresh wild Alaskan salmon, rubbed in a bbq
           brown sugar rub, baked for 25 minutes on a bed of pineapple, and garnished in butter, garlic, and chives. You won’t want to miss it!
-        </h3>
+        </p>
         <Link to="/recipe/3">
           <button className="blue-btn">Check it out</button>
         </Link>
